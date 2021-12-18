83a9ecc9f91cb4108ba3d35631f73276f5a06e89
AkashDeep000 <akashdeepdeepdas002@gmail.com>
2021-12-19 00:52:28 +0530

    api time test index error fix

--- pages/ringtone/browse/index.js
+++ pages/ringtone/browse/index.js
@@ -12,20 +12,6 @@ export default function Home({data}) {
 
 // This function gets called at build time
 export async function getStaticProps() {
-  // Call an external API endpoint to get posts
-  //const res = await fetch('')
-  //const posts = await res.json()
-
- // const data = searchData({
- //   searchTerm : "xyz"
- // })
-  const data = await googleSuggest({
-    searchTerm : "parinita",
-    country: "in"
-  })
-  console.log(data)
-  // By returning { props: { posts } }, the Blog component
-  // will receive `posts` as a prop at build time
   return {
     props: {
       data : "data"
